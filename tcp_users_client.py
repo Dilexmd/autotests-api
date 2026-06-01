75da8003d26997a324cba3470b76824e17e95efd
Dilexmd <lexmd_work@mail.ru>
2026-06-01 18:04:05 +0300

    Знакомство с TCP/IP протоколами.Практика работы с TCP\IP протоколом

---
+++ tcp_users_client.py
@@ -0,0 +1,18 @@
+import socket
+
+def client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = ('localhost', 12345)
+    client_socket.connect(server_address)
+
+    message = "Как дела сервер?"
+    client_socket.send(message.encode())
+
+    response = client_socket.recv(1024).decode()
+    print(f"Ответ от сервера: {response}")
+
+    client_socket.close()
+
+if __name__ == '__main__':
+    client()
