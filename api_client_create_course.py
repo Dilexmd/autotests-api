ca97e70903ee919c57e8d7c368638587516ba288
Dilexmd <lexmd_work@mail.ru>
2026-07-17 18:27:06 +0300

    применяем Pydantic в API клиентах

--- api_client_create_course.py
+++ api_client_create_course.py
@@ -1,7 +1,9 @@
-from clients.files.files_client import get_files_client, CreateFileRequestDict
+from clients.files.files_client import get_files_client
+from clients.files.files_schema import CreateFileRequestSchema
 from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
-from clients.private_http_builder import AuthenticationUserDict
-from clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema
 from tools.fakers import get_random_email
 
 
@@ -14,17 +16,19 @@ public_users_client = get_public_users_client()
 """
 Создание нового пользователя
 """
-create_user_request = CreateUserRequestDict(
+create_user_request = CreateUserRequestSchema(
     email=get_random_email(),
     password="string",
-    lastName="string",
-    firstName="string",
-    middleName="string"
+    last_name="string",
+    first_name="string",
+    middle_name="string"
 )
+
 create_user_response = public_users_client.create_user(create_user_request)
-authentication_user = AuthenticationUserDict(
-        email=create_user_request['email'],
-        password=create_user_request['password']
+
+authentication_user = AuthenticationUserSchema(
+        email=create_user_request.email,
+        password=create_user_request.password
 )
 
 
@@ -38,7 +42,7 @@ courses_client = get_courses_client(authentication_user)
 """
 Загрузка нового файла
 """
-create_file_request = CreateFileRequestDict(
+create_file_request = CreateFileRequestSchema(
     filename="image.png",
     directory="courses",
     upload_file="./testdata/files/image.png"
@@ -57,8 +61,8 @@ create_course_request = CreateCourseRequestDict (
     minScore=10,
     description="Python API Course",
     estimatedTime="2 weeks",
-    previewFileId=create_file_response['file']['id'],
-    createdByUserId=create_user_response['user']['id']
+    previewFileId=create_file_response.file.id,
+    createdByUserId=create_user_response.user.id
 
 )
 create_course_response = courses_client.create_course(create_course_request)
--- clients/users/private_users_client.py
+++ clients/users/private_users_client.py
@@ -1,35 +1,9 @@
-from clients.api_client import APIClient
 from httpx import Response
-from typing import TypedDict
 
-from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
+from clients.api_client import APIClient
+from clients.private_http_builder import AuthenticationUserSchema, get_private_http_client
+from clients.users.users_schema import GetUserResponseSchema,UpdateUserRequestSchema
 
-
-class User(TypedDict):
-    """
-    Описание структуры запроса для создания пользователя.
-    """
-    id: str
-    email: str
-    lastName: str
-    firstName: str
-    middleName: str
-
-
-class GetUserResponseDict(TypedDict):
-    """
-    Описание структуры ответа на получение пользователя.
-    """
-    user: User
-
-class UpdateUserRequestDict(TypedDict):
-    """
-    Описание структуры запроса на обновление пользователя.
-    """
-    email: str | None
-    lastName: str | None
-    firstName: str | None
-    middleName: str | None
 
 class PrivateUsersClient(APIClient):
     """
@@ -52,7 +26,7 @@ class PrivateUsersClient(APIClient):
         """
         return self.get(f'/api/v1/users/{user_id}')
 
-    def update_user_api(self, user_id: str,request: UpdateUserRequestDict) -> Response:
+    def update_user_api(self, user_id: str,request: UpdateUserRequestSchema) -> Response:
         """
         Метод обновления пользователя по идентификатору.
 
@@ -60,7 +34,7 @@ class PrivateUsersClient(APIClient):
         :param request: Словарь с email, lastName, firstName, middleName.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.patch(f'/api/v1/users/{user_id}', json=request)
+        return self.patch(f'/api/v1/users/{user_id}', json=request.model_dump(by))
 
     def delete_user_api(self, user_id: str) -> Response:
         """
@@ -71,12 +45,12 @@ class PrivateUsersClient(APIClient):
         """
         return self.delete(f'/api/v1/users/{user_id}')
 
-    def get_user(self,user_id:str) -> GetUserResponseDict:
+    def get_user(self,user_id:str) -> GetUserResponseSchema:
         response = self.get_user_api(user_id)
-        return response.json()
+        return GetUserResponseSchema.model_validate_json(response.text)
 
 
-def get_private_users_client(user:AuthenticationUserDict)-> PrivateUsersClient:
+def get_private_users_client(user:AuthenticationUserSchema)-> PrivateUsersClient:
     """
        Функция создает экземпляр PrivateUsersClient с уже настроенным HTTP-клиентом
 
