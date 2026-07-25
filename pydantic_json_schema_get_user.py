d421c6b639f63be307f39be8fe83f8320a95628a
Dilexmd <lexmd_work@mail.ru>
2026-07-25 15:35:02 +0300

    валидация JSON schema.практикуемся в работе с JSON schema

---
+++ pydantic_json_schema_get_user.py
@@ -0,0 +1,36 @@
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import get_private_users_client
+from clients.users.public_users_client import get_public_users_client
+from tools.assertions.schema import validate_json_schema
+from tools.fakers import get_random_email
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+
+public_users_client = get_public_users_client()
+
+
+create_user_request = CreateUserRequestSchema(
+    email=get_random_email(),
+    password="string",
+    last_name="string",
+    first_name="string",
+    middle_name="string"
+)
+
+create_user_response = public_users_client.create_user_api(create_user_request)
+create_user_response_schema = CreateUserResponseSchema.model_json_schema()
+validate_json_schema(instance=create_user_response.json(),schema=create_user_response_schema)
+
+user_id = create_user_response.json()["user"]["id"]
+user_email = create_user_request.email
+
+auth_user = AuthenticationUserSchema(
+    email=user_email,
+    password="string"
+)
+
+private_users_client = get_private_users_client(auth_user)
+
+get_user_data_response = private_users_client.get_user_api(user_id)
+get_user_data_response_schema = GetUserResponseSchema.model_json_schema()
+
+validate_json_schema(instance=get_user_data_response.json(),schema=get_user_data_response_schema)
