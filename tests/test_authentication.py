4519fc70150ad8f67ff8ca6b6e0d07b456de12b6
Dilexmd <lexmd_work@mail.ru>
2026-08-04 07:41:42 +0300

    Фикстуры Pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -3,9 +3,8 @@ from http import HTTPStatus
 import pytest
 
 from clients.authentication.authentication_schema import LoginResponseSchema, LoginRequestSchema
-from clients.users.users_schema import CreateUserRequestSchema
-from clients.authentication.authentication_client import get_authentication_client
-from clients.users.public_users_client import get_public_users_client
+from clients.authentication.authentication_client import  AuthenticationClient
+from tests.conftest import UserFixture
 
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
@@ -14,22 +13,12 @@ from tools.assertions.schema import validate_json_schema
 
 @pytest.mark.regression
 @pytest.mark.authentication
-def test_login():
-    public_user_client = get_public_users_client()
-    authentication_user_client = get_authentication_client()
+def test_login(function_user: UserFixture,authentication_client: AuthenticationClient):
+    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+    response = authentication_client.login_api(request)
+    response_data = LoginResponseSchema.model_validate_json(response.text)
 
-    request = CreateUserRequestSchema()
-    public_user_client.create_user(request)
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_login_response(response_data)
 
-    login_request = LoginRequestSchema(
-         email=request.email,
-         password=request.password
-    )
-
-    login_response = authentication_user_client.login_api(login_request)
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
-
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
-    assert_login_response(login_response_data)
-
-    validate_json_schema(login_response.json(),login_response_data.model_json_schema())
+    validate_json_schema(response.json(), response_data.model_json_schema())
